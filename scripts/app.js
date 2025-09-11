63d092d54937877490ea78343f83d30bcf2dd0ba
erfanhoush <erfanhoushyarkhah@gmail.com>
2025-09-11 22:57:37 +0330

    ridan

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,8 @@
 const swiper = new Swiper(".story", {
-  slidesPerView: 11.5,
+  slidesPerView: 10,
   spaceBetween: 39,
-  centeredSlide: true,
   navigation: {
     nextEl: ".next",
     prevEl: ".prev"
-  },
-});
-let swiper_ad = new Swiper(".mySwiper_ad", {
-  pagination: {
-    el: ".custom-pagination-ad",
-    clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + ' custom-pagination-ad-bullet"></span>';
-    }
   },
 });
---
+++ scripts/sliderSwiper.js
@@ -0,0 +1,17 @@
+const sliderSwiper = new Swiper(".slider", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  pagination: {
+    el: ".custom-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + ' custom-pagination-bullet"></span>';
+    }
+  },
+  navigation: {
+    nextEl: ".next-nav",
+    prevEl: ".prev-nav"
+  },
+});
