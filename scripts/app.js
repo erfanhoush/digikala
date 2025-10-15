82f8d859a180ea512421a740be8fe35fc76b17aa
erfanhoush <erfanhoushyarkhah@gmail.com>
2025-10-15 16:56:46 +0330

    email

--- scripts/app.js
+++ scripts/app.js
@@ -15,3 +15,40 @@ homeBtn.addEventListener("click", () => {
     behavior: "smooth"
   });
 });
+
+
+const emailInput = document.querySelector(".email-box");
+const emailInputInvalidText = document.querySelector(".invalid-email__box");
+const emailInputEmpty = document.querySelector(".empty-email__box");
+const submitBtn = document.querySelector(".submit");
+
+
+const gmailRegex = /^(?!.*\.\.)[a-zA-Z0-9](?:[a-zA-Z0-9._+%-]{0,62}[a-zA-Z0-9+%-])?@gmail\.com$/i;
+
+let emailInputValue = "";
+
+const isValidGmail = (email) => {
+  return gmailRegex.test(email);
+}
+
+
+emailInput.addEventListener("blur", () => {
+  if (emailInput.length === 0) {
+    console.log(emailInput.length);
+    
+    emailInputEmpty.classList.add("empty-alert");
+  }
+  emailInputEmpty.classList.add("empty-alert");
+  emailInputEmpty.style.display = "block"
+});
+
+submitBtn.addEventListener("click", () => {
+  if (!isValidGmail(emailInput.value) && emailInput.length > 0) {
+    emailInputInvalidText.classList.add("invalid-alert");
+    emailInputInvalidText.style.display = "block"
+  }
+});
+
+emailInput.addEventListener("keyup", () => {
+  emailInputEmpty.style.display = "block"
+})
